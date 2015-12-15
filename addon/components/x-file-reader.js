a1b0b36d1663e8ff114abbdce0d57fd9c213442f
Charles Lowell <cowboyd@frontside.io>
2015-12-15 15:05:28 -0600

    update to use the file reader state

--- addon/components/x-file-reader.js
+++ addon/components/x-file-reader.js
@@ -1,18 +1,31 @@
 import Ember from 'ember';
 import layout from '../templates/components/x-file-reader';
 
+import State from '../file-reader';
+
 export default Ember.Component.extend({
   layout: layout,
   classNames: ["spec-file-reader"],
   file: null,
-  model: {result: null, style: ""},
+  model: new State(),
   readFile: Ember.observer('file', function() {
-    console.log('readFile');
     let file = this.get('file');
     let reader = new FileReader();
-    this.set('model', {result: null, style: ""});
-    reader.onloadend = Ember.run.bind(this, function() {
-      this.set('model', {result: reader.result, style:`background-image: url(${reader.result})`});
+    this.set('model', new State());
+    reader.onloadstart = Ember.run.bind(this, function(event) {
+      this.set('model', this.get('model').loadstart(event));
+    });
+    reader.onprogress = Ember.run.bind(this, function(event) {
+      this.set('model', this.get('model').progress(event));
+    });
+    reader.onloadend = Ember.run.bind(this, function(event) {
+      this.set('model', this.get('model').loadend(event));
+    });
+    reader.onabort = Ember.run.bind(this, function(event) {
+      this.set('model', this.get('model').abort(event));
+    });
+    reader.onerror = Ember.run.bind(this, function(event) {
+      this.set('model', this.get('model').error(event));
     });
     reader.readAsDataURL(file);
   })
